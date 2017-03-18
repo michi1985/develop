d295bac4d305be4439a5018026c8dc44f82e3e7b
michi1985 <acoustic.revolution0322@gmail.com>
2017-03-18 12:55:38 +0900

    カテゴリの追加

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,7 @@
 class Potepan::ProductsController < ApplicationController
     def index
         @products = Spree::Product.all
+        @prototypes = Spree::Prototype.all
     end
     def show
         @single_product = Spree::Product.find(params[:id])
